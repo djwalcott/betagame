1c9c0413db748cf7153ec5de00df9454b8e3b212
Jacob Hummel <jacobwhummel@gmail.com>
2021-08-16 01:20:50 +0300

    Seed 2021 NFL games

---
+++ migrations/20210815221608-seed-nfl-2021.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const fs = require('fs');
+const parse = require('csv-parse/lib/sync')
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = async function(db) {
+  const games = parse(fs.readFileSync('./seed_data/nfl_2021.csv'));
+
+  for (const game of games) {
+    await db.insert('sports_games', [
+      'season',
+      'week',
+      'start_time',
+      'home_team_short_name',
+      'away_team_short_name'
+    ], [
+      2021,
+      game[0],
+      game[1],
+      game[2],
+      game[3]
+    ]);
+  }
+
+  return null;
+};
+
+exports.down = function(db) {
+  return null;
+};
+
+exports._meta = {
+  "version": 1
+};
